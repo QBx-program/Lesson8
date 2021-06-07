84fca1caadbe431bdf0a09664267a84e422085eb
QBx <ortynskiy-k.ros@mail.ru>
2021-06-08 01:01:16 +0300

    Исправлена ошибка

--- 1_2.py
+++ 1_2.py
@@ -15,27 +15,24 @@ with open('parse.txt', 'w', encoding='UTF-8') as f:
 
 with open('parse.txt', 'r', encoding='UTF-8') as f:
     line = f.readline()
-    while line:
+    i = 0
+    while True:
         work_line = []
         line = f.readline().strip()
-        remote_addr = re.compile(r'^[\d+\.+]+')
-        remote_addr_2 = re.compile(r'^([\:+\d+\w+]+)')
-        request_datetime = re.compile(r'\[(.+)\]')
-        request_type = re.compile(r'\"(\w+)\s[/]')
-        requested_resource = re.compile(r'\s(\/.+)\sHTTP')
-        response_code = re.compile(r'\"\s(\d+)\s\d+')
-        response_size = re.compile(r'\"\s\d+\s(\d+)')
-        try:
-            if remote_addr.match(line):
-                work_line.append(re.findall(remote_addr, line)[0])
-            else:
-                work_line.append(re.findall(remote_addr_2, line)[0])
+        if line:
+            remote_addr = re.compile(r'^[\w+\d+\:+\.]+')
+            request_datetime = re.compile(r'\[(.+)\]')
+            request_type = re.compile(r'\"(\w+)\s[/]')
+            requested_resource = re.compile(r'\s(\/.+)\sHTTP')
+            response_code = re.compile(r'\"\s(\d+)\s\d+')
+            response_size = re.compile(r'\"\s\d+\s(\d+)')
+            work_line.append(re.findall(remote_addr, line)[0])
             work_line.append(re.findall(request_datetime, line)[0])
             work_line.append(re.findall(request_type, line)[0])
             work_line.append(re.findall(requested_resource, line)[0])
             work_line.append(re.findall(response_code, line)[0])
             work_line.append(re.findall(response_size, line)[0])
             parsed_raw = tuple(work_line)
-        except Exception as e:
-            print(e)
-        print(parsed_raw)
+            print(parsed_raw)
+
+
