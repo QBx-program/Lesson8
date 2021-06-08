2edfce5364a5e63e50295ad8e7bf9e8e482551ee
QBx <ortynskiy-k.ros@mail.ru>
2021-06-08 19:08:45 +0300

    2222

--- 4.py
+++ 4.py
@@ -1,10 +1,10 @@
 # Задание 4
 
-def val_checker(verbosity=0):
+def val_checker(verbosity=False):
     def get_num(func):
-        def validator(*args):
-            num = func(*args)
-            if verbosity(*args):
+        def validator(args):
+            num = func(args)
+            if verbosity(args):
                 return num
             else:
                 raise ValueError(f'ValueError: wrong val {args}')
@@ -17,6 +17,6 @@ def val_checker(verbosity=0):
 def calc_cube(x):
     return x ** 3
 
-a = calc_cube(10)
+a = calc_cube(-5)
 print(a)
 print(calc_cube.__name__)
